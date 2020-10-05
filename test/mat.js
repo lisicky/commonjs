0b303dc0726c20e5603cb855cf9ff5204a822703
Gordon <gordonlarrigan@gmail.com>
2020-10-05 22:58:46 +0100

    Add vector and matrix tests

--- test/mat.js
+++ test/mat.js
@@ -2,10 +2,169 @@ const { mat } = require('../common.js');
 
 QUnit.module('Matrix tests');
 
-QUnit.test('Invert a matrix', assert => {
-  assert.deepEqual(mat.inv(mat(3, 3, [7, 2, 1, 0, 3, -1, -3, 4, -2])), {
+QUnit.test('Initialise a matrix', assert => {
+
+  // Initialise with no arguments, should get a 4x4 matrix
+  assert.deepEqual(mat(), {
+    m: 4,
+    n: 4,
+    entries: [
+      0, 0, 0, 0,
+      0, 0, 0, 0,
+      0, 0, 0, 0,
+      0, 0, 0, 0
+    ]
+  });
+
+  // Initialise with size arguments, should get a 3x3 matrix
+  assert.deepEqual(mat(3, 3), {
     m: 3,
     n: 3,
-    entries: [-2, 8, -5, 3, -11, 7, 9, -34, 21]
+    entries: [
+      0, 0, 0,
+      0, 0, 0,
+      0, 0, 0
+    ]
   });
+
+  // Initialise with all entries populated
+  assert.deepEqual(mat(3, 3, [1, 2, 3, 4, 5, 6, 7, 8, 9]), {
+    m: 3,
+    n: 3,
+    entries: [
+      1, 2, 3,
+      4, 5, 6,
+      7, 8, 9
+    ]
+  });
+
+  // Initialise with some entries populated
+  assert.deepEqual(mat(3, 3, [1, 2, 3]), {
+    m: 3,
+    n: 3,
+    entries: [
+      1, 2, 3,
+      0, 0, 0,
+      0, 0, 0
+    ]
+  });
+});
+
+QUnit.test('Identity matrix', assert => {
+
+  // 2x2 identity matrix
+  assert.deepEqual(mat.identity(2), {
+    m: 2,
+    n: 2,
+    entries: [
+      1, 0,
+      0, 1
+    ]
+  });
+
+  // 3x3 identity matrix
+  assert.deepEqual(mat.identity(3), {
+    m: 3,
+    n: 3,
+    entries: [
+      1, 0, 0,
+      0, 1, 0,
+      0, 0, 1
+    ]
+  });
+
+  // 4x4 identity matrix
+  assert.deepEqual(mat.identity(4), {
+    m: 4,
+    n: 4,
+    entries: [
+      1, 0, 0, 0,
+      0, 1, 0, 0,
+      0, 0, 1, 0,
+      0, 0, 0, 1
+    ]
+  });
+});
+
+QUnit.test('Get and set matrix entries', assert => {
+  const a = mat(3, 3, Array.range(9));
+
+  // Get
+  assert.equal(mat.get(a, 1, 1), 0);
+  assert.equal(mat.get(a, 3, 3), 8);
+
+  // Set
+  mat.set(a, 1, 1, 10);
+  assert.equal(mat.get(a, 1, 1), 10);
+
+  mat.set(a, 3, 2, 11);
+  assert.equal(mat.get(a, 3, 2), 11);
+});
+
+QUnit.test('Get matrix rows/columns', assert => {
+  const a = mat(4, 4, Array.range(16));
+
+  // Rows
+  assert.deepEqual(mat.row(a, 1), [0, 1, 2, 3]);
+  assert.deepEqual(mat.row(a, 4), [12, 13, 14, 15]);
+
+  // Columns
+  assert.deepEqual(mat.col(a, 1), [0, 4, 8, 12]);
+  assert.deepEqual(mat.col(a, 4), [3, 7, 11, 15]);
+});
+
+QUnit.test('Matrix addition', assert => {
+  assert.equal(1, 1);
+});
+
+QUnit.test('Matrix subtraction', assert => {
+  assert.equal(1, 1);
+});
+
+QUnit.test('Matrix multiplication', assert => {
+  assert.equal(1, 1);
+});
+
+QUnit.test('Matrix scalar multiplication', assert => {
+  assert.equal(1, 1);
+});
+
+QUnit.test('Matrix transposition', assert => {
+  assert.equal(1, 1);
+});
+
+QUnit.test('Matrix minor', assert => {
+  assert.equal(1, 1);
+});
+
+QUnit.test('Matrix determinant', assert => {
+  assert.equal(1, 1);
+});
+
+QUnit.test('Normalised matrix', assert => {
+  assert.equal(1, 1);
+});
+
+QUnit.test('Matrix adjugate', assert => {
+  assert.equal(1, 1);
+});
+
+QUnit.test('Matrix inverse', assert => {
+  assert.equal(1, 1);
+});
+
+QUnit.test('Matrix equality', assert => {
+  assert.equal(1, 1);
+});
+
+QUnit.test('Copy a matrix', assert => {
+  assert.equal(1, 1);
+});
+
+QUnit.test('Mapping a matrix', assert => {
+  assert.equal(1, 1);
+});
+
+QUnit.test('Matrix string conversion', assert => {
+  assert.equal(1, 1);
 });
--- test/vec.js
+++ test/vec.js
@@ -2,6 +2,116 @@ const { vec } = require('../common.js');
 
 QUnit.module('Vector tests');
 
-QUnit.test('Scale a vector', assert => {
-  assert.deepEqual(vec.mul(vec(2, 3), 2), { x: 4, y: 6 });
+QUnit.test('Initialise a vector', assert => {
+
+  // Initialise with no arguments, should get a zero-vector
+  assert.deepEqual(vec(), { x: 0, y: 0 });
+
+  // Initialise with both arguments
+  assert.deepEqual(vec(2, 3), { x: 2, y: 3 });
+
+  // Initialise with 1 argument, both components should be equal
+  assert.deepEqual(vec(4), { x: 4, y: 4 });
+
+  // Initialise with another vector, should create a copy
+  const originalVector = vec(5, 6);
+  const copiedVector = vec(originalVector);
+  assert.deepEqual(copiedVector, { x: 5, y: 6 });
+
+  // Make sure the copied vector isn't still pointing at the original vector
+  originalVector.x = 7;
+  assert.equal(copiedVector.x, 5);
+});
+
+QUnit.test('Vector components', assert => {
+  const a = vec(1, 2);
+  const components = vec.components(a);
+  assert.equal(components[0], 1);
+  assert.equal(components[1], 2);
+});
+
+QUnit.test('Unit vectors', assert => {
+  assert.deepEqual(vec.ux(), { x: 1, y: 0 });
+  assert.deepEqual(vec.uy(), { x: 0, y: 1 });
+});
+
+QUnit.test('Vector addition', assert => {
+  const a = vec(1, 1);
+  const b = vec(2, 3);
+  assert.deepEqual(vec.add(a, b), { x: 3, y: 4 });
+});
+
+QUnit.test('Vector subtraction', assert => {
+  const a = vec(1, 1);
+  const b = vec(2, 3);
+  assert.deepEqual(vec.sub(a, b), { x: -1, y: -2 });
+});
+
+QUnit.test('Vector multiplication', assert => {
+  const a = vec(2, 3);
+  const b = 2;
+  assert.deepEqual(vec.mul(a, b), { x: 4, y: 6 });
+});
+
+QUnit.test('Vector length', assert => {
+  const a = vec(2, 2);
+  assert.equal(vec.len(a), Math.sqrt(8));
+});
+
+QUnit.test('Vector length (manhattan)', assert => {
+  const a = vec(2, 2);
+  assert.equal(vec.manhattan(a), 4);
+});
+
+QUnit.test('Normalised vector', assert => {
+  const a = vec(2, 3);
+  assert.deepEqual(vec.nor(a), {
+    x: 0.5547001962252291,
+    y: 0.8320502943378437
+  });
+});
+
+QUnit.test('Vector dot product', assert => {
+  const a = vec(2, 3);
+  const b = vec(4, 5);
+  assert.equal(vec.dot(a, b), 23);
+});
+
+QUnit.test('Vector rotation', assert => {
+  const a = vec(1, 0);
+  const rotatedVector = vec.rot(a, Math.PI / 2);
+  assert.equal(Math.floatEquals(rotatedVector.x, 0), true);
+  assert.equal(Math.floatEquals(rotatedVector.y, 1), true);
+});
+
+QUnit.test('Vector equality', assert => {
+  const a = vec(2, 2);
+  const b = vec(2, 2);
+  assert.equal(vec.eq(a, b), true);
+});
+
+QUnit.test('Vector angle in radians', assert => {
+  const a = vec(0, 1);
+  assert.equal(vec.rad(a), Math.PI / 2);
+});
+
+QUnit.test('Copy a vector', assert => {
+  const originalVector = vec(2, 3);
+  const copiedVector = vec.cpy(originalVector);
+  assert.deepEqual(originalVector, copiedVector);
+
+  // Make sure the copied vector isn't still pointing at the original vector
+  originalVector.x = 4;
+  assert.equal(copiedVector.x, 2);
+});
+
+QUnit.test('Mapping a vector', assert => {
+  const a = vec(2, 3);
+  assert.deepEqual(vec.map(a, v => v + 1), { x: 3, y: 4 });
+});
+
+QUnit.test('Vector string conversion', assert => {
+  const a = vec(2, 3);
+  assert.equal(vec.str(a), '2, 3');
+  assert.equal(vec.str(a, ':'), '2:3');
 });
