6ab5b7b02c3634706b4072ba4c2a8b3f801d6f60
Gordon <gordonlarrigan@gmail.com>
2020-10-07 13:55:53 +0100

    Improve jsdoc annotations

--- common.js
+++ common.js
@@ -14,7 +14,7 @@
  * Create a new vector
  * @param {number|vec} [x] The x component of the vector, or a vector to copy
  * @param {number} [y] The y component of the vector
- * @return {vec}
+ * @return {vec} A new vector
  * @example <caption>Various ways to initialise a vector</caption>
  * let a = vec(3, 2);  // (3, 2)
  * let b = vec(4);     // (4, 4)
@@ -22,7 +22,7 @@
  * let d = vec();      // (0, 0)
  */
 const vec = (x, y) => (!x && !y ?
-  { x: 0, y: 0 } : (typeof x == 'object' ?
+  { x: 0, y: 0 } : (typeof x === 'object' ?
     { x: x.x || 0, y: x.y || 0 } : (y === null || y === undefined ?
       { x: x, y: x } : { x: x, y: y })
   )
@@ -31,64 +31,64 @@ const vec = (x, y) => (!x && !y ?
 /**
  * Get the components of a vector as an array
  * @param {vec} a The vector to get components from
- * @return {Array<number>}
+ * @return {Array<number>} The vector components as an array
  */
 vec.components = a => [a.x, a.y];
 
 /**
  * Return a unit vector (1, 0)
- * @return {vec}
+ * @return {vec} A unit vector (1, 0)
  */
 vec.ux = () => vec(1, 0);
 
 /**
  * Return a unit vector (0, 1)
- * @return {vec}
+ * @return {vec} A unit vector (0, 1)
  */
 vec.uy = () => vec(0, 1);
 
 /**
  * Add vectors
- * @param {vec} a
- * @param {vec} b
+ * @param {vec} a Vector a
+ * @param {vec} b Vector b
  * @return {vec} a + b
  */
 vec.add = (a, b) => ({ x: a.x + b.x, y: a.y + b.y });
 
 /**
  * Scale a vector
- * @param {vec} a
- * @param {number} b
+ * @param {vec} a Vector a
+ * @param {number} b Scalar b
  * @return {vec} a * b
  */
 vec.mul = (a, b) => ({ x: a.x * b, y: a.y * b });
 
 /**
  * Subtract vectors
- * @param {vec} a
- * @param {vec} b
+ * @param {vec} a Vector a
+ * @param {vec} b Vector b
  * @return {vec} a - b
  */
 vec.sub = (a, b) => ({ x: a.x - b.x, y: a.y - b.y });
 
 /**
  * Get the length of a vector
- * @param {vec} a
+ * @param {vec} a Vector a
  * @return {vec} |a|
  */
 vec.len = a => Math.sqrt(a.x * a.x + a.y * a.y);
 
 /**
  * Get the length of a vector using taxicab geometry
- * @param {vec} a
+ * @param {vec} a Vector a
  * @return {vec} |a|
  */
 vec.manhattan = a => Math.abs(a.x) + Math.abs(a.y);
 
 /**
  * Normalise a vector
- * @param {vec} a
- * @return {vec}
+ * @param {vec} a The vector to normalise
+ * @return {vec} A normalised vector
  */
 vec.nor = a => {
   let len = vec.len(a);
@@ -97,17 +97,17 @@ vec.nor = a => {
 
 /**
  * Get a dot product of vectors
- * @param {vec} a
- * @param {vec} b
+ * @param {vec} a Vector a
+ * @param {vec} b Vector b
  * @return {vec} a ∙ b
  */
 vec.dot = (a, b) => a.x * b.x + a.y * b.y;
 
 /**
  * Rotate a vector by r radians
- * @param {vec} a
+ * @param {vec} a The vector to rotate
  * @param {number} r The angle to rotate by, measured in radians
- * @return {vec}
+ * @return {vec} A rotated vector
  */
 vec.rot = (a, r) => {
   let s = Math.sin(r),
@@ -117,23 +117,23 @@ vec.rot = (a, r) => {
 
 /**
  * Check if two vectors are equal
- * @param {vec} a
- * @param {vec} b
- * @return {boolean} True if the vectors are equal, false otherwise
+ * @param {vec} a Vector a
+ * @param {vec} b Vector b
+ * @return {boolean} True if vectors a and b are equal, false otherwise
  */
-vec.eq = (a, b) => a.x == b.x && a.y == b.y;
+vec.eq = (a, b) => a.x === b.x && a.y === b.y;
 
 /**
  * Get the angle of a vector
- * @param {vec} a
- * @return {number} The angle of the vector in radians
+ * @param {vec} a Vector a
+ * @return {number} The angle of vector a in radians
  */
 vec.rad = a => Math.atan2(a.y, a.x);
 
 /**
  * Copy a vector
- * @param {vec} a
- * @return {vec} A copy of a
+ * @param {vec} a The vector to copy
+ * @return {vec} A copy of vector a
  */
 vec.cpy = a => vec(a);
 
@@ -147,17 +147,17 @@ vec.cpy = a => vec(a);
 
 /**
  * Call a function on each component of a vector and build a new vector from the results
- * @param {vec} a
+ * @param {vec} a Vector a
  * @param {vectorMapCallback} f The function to call on each component of the vector
- * @return {vec} The mapped vector
+ * @return {vec} Vector a mapped through f
  */
 vec.map = (a, f) => ({ x: f(a.x, 'x'), y: f(a.y, 'y') });
 
 /**
  * Convert a vector into a string
- * @param {vec} a
+ * @param {vec} a The vector to convert
  * @param {string} [s=', '] The separator string
- * @return {string}
+ * @return {string} A string representation of the vector
  */
 vec.str = (a, s = ', ') => `${a.x}${s}${a.y}`;
 
@@ -174,7 +174,7 @@ vec.str = (a, s = ', ') => `${a.x}${s}${a.y}`;
  * @param {number} m The number of rows
  * @param {number} n The number of columns
  * @param {Array<number>} entries Matrix values in reading order
- * @return {mat}
+ * @return {mat} A new matrix
  */
 const mat = (m = 4, n = 4, entries = []) => ({
   m, n,
@@ -184,33 +184,33 @@ const mat = (m = 4, n = 4, entries = []) => ({
 /**
  * Get an identity matrix of size n
  * @param {number} n The size of the matrix
- * @return {mat}
+ * @return {mat} An identity matrix
  */
-mat.identity = n => mat(n, n, Array(n * n).fill(0).map((v, i) => +(Math.floor(i / n) == i % n)));
+mat.identity = n => mat(n, n, Array(n * n).fill(0).map((v, i) => +(Math.floor(i / n) === i % n)));
 
 /**
  * Get an entry from a matrix
- * @param {mat} a
+ * @param {mat} a Matrix a
  * @param {number} i The row offset
  * @param {number} j The column offset
- * @return {number} The value at position (i, j) in the matrix
+ * @return {number} The value at position (i, j) in matrix a
  */
 mat.get = (a, i, j) => a.entries[(j - 1) + (i - 1) * a.n];
 
 /**
  * Set an entry of a matrix
- * @param {mat} a
+ * @param {mat} a Matrix a
  * @param {number} i The row offset
  * @param {number} j The column offset
- * @param {number} v The value to set
+ * @param {number} v The value to set in matrix a
  */
 mat.set = (a, i, j, v) => { a.entries[(j - 1) + (i - 1) * a.n] = v; };
 
 /**
  * Get a row from a matrix as an array
- * @param {mat} a
+ * @param {mat} a Matrix a
  * @param {number} m The row offset
- * @return {Array<number>} Row m from the matrix
+ * @return {Array<number>} Row m from matrix a
  */
 mat.row = (a, m) => {
   const s = (m - 1) * a.n;
@@ -219,32 +219,32 @@ mat.row = (a, m) => {
 
 /**
  * Get a column from a matrix as an array
- * @param {mat} a
+ * @param {mat} a Matrix a
  * @param {number} n The column offset
- * @return {Array<number>} Column n from the matrix
+ * @return {Array<number>} Column n from matrix a
  */
 mat.col = (a, n) => Array.times(i => mat.get(a, (i + 1), n), a.m);
 
 /**
  * Add matrices
- * @param {mat} a
- * @param {mat} b
+ * @param {mat} a Matrix a
+ * @param {mat} b Matrix b
  * @return {mat} a + b
  */
-mat.add = (a, b) => a.m == b.m && a.n == b.n && mat.map(a, (v, i) => v + b.entries[i]);
+mat.add = (a, b) => a.m === b.m && a.n === b.n && mat.map(a, (v, i) => v + b.entries[i]);
 
 /**
  * Subtract matrices
- * @param {mat} a
- * @param {mat} b
+ * @param {mat} a Matrix a
+ * @param {mat} b Matrix b
  * @return {mat} a - b
  */
-mat.sub = (a, b) => a.m == b.m && a.n == b.n && mat.map(a, (v, i) => v - b.entries[i]);
+mat.sub = (a, b) => a.m === b.m && a.n === b.n && mat.map(a, (v, i) => v - b.entries[i]);
 
 /**
  * Multiply matrices
- * @param {mat} a
- * @param {mat} b
+ * @param {mat} a Matrix a
+ * @param {mat} b Matrix b
  * @return {mat} ab
  */
 mat.mul = (a, b) => {
@@ -260,33 +260,33 @@ mat.mul = (a, b) => {
 
 /**
  * Scale a matrix
- * @param {mat} a
- * @param {number} b
+ * @param {mat} a Matrix a
+ * @param {number} b Scalar b
  * @return {mat} a * b
  */
 mat.scale = (a, b) => mat.map(a, v => v * b);
 
 /**
  * Transpose a matrix
- * @param {mat} a
- * @return {mat}
+ * @param {mat} a The matrix to transpose
+ * @return {mat} A transposed matrix
  */
 mat.trans = a => mat(a.n, a.m, Array.times(i => mat.col(a, (i + 1)), a.n).flat());
 
 /**
  * Get the minor of a matrix
- * @param {mat} a
+ * @param {mat} a Matrix a
  * @param {number} i The row offset
  * @param {number} j The column offset
- * @return {mat} The (i, j) minor of the matrix
+ * @return {mat} The (i, j) minor of matrix a
  */
 mat.minor = (a, i, j) => {
   if (a.m !== a.n) { return false; }
   const entries = [];
   for (let ii = 1; ii <= a.m; ii++) {
-    if (ii == i) { continue; }
+    if (ii === i) { continue; }
     for (let jj = 1; jj <= a.n; jj++) {
-      if (jj == j) { continue; }
+      if (jj === j) { continue; }
       entries.push(mat.get(a, ii, jj));
     }
   }
@@ -295,15 +295,15 @@ mat.minor = (a, i, j) => {
 
 /**
  * Get the determinant of a matrix
- * @param {mat} a
- * @return {number} The matrix determinant
+ * @param {mat} a Matrix a
+ * @return {number} The determinant of matrix a
  */
 mat.det = a => {
   if (a.m !== a.n) { return false; }
-  if (a.m == 1) {
+  if (a.m === 1) {
     return a.entries[0];
   }
-  if (a.m == 2) {
+  if (a.m === 2) {
     return a.entries[0] * a.entries[3] - a.entries[1] * a.entries[2];
   }
   let total = 0, sign = 1;
@@ -316,8 +316,8 @@ mat.det = a => {
 
 /**
  * Normalise a matrix
- * @param {mat} a
- * @return {mat}
+ * @param {mat} a The matrix to normalise
+ * @return {mat} A normalised matrix
  */
 mat.nor = a => {
   if (a.m !== a.n) { return false; }
@@ -327,8 +327,8 @@ mat.nor = a => {
 
 /**
  * Get the adjugate of a matrix
- * @param {mat} a
- * @return {mat}
+ * @param {mat} a The matrix from which to get the adjugate
+ * @return {mat} An adjugate matrix
  */
 mat.adj = a => {
   const minors = mat(a.m, a.n);
@@ -343,28 +343,28 @@ mat.adj = a => {
 
 /**
  * Get the inverse of a matrix
- * @param {mat} a
- * @return {mat}
+ * @param {mat} a The matrix to invert
+ * @return {mat} An inverse matrix
  */
 mat.inv = a => {
   if (a.m !== a.n) { return false; }
   const d = mat.det(a);
-  if (d == 0) { return false; }
+  if (d === 0) { return false; }
   return mat.scale(mat.adj(a), 1 / d);
 };
 
 /**
  * Check if two matrices are equal
- * @param {mat} a
- * @param {mat} b
- * @return {boolean} True if the matrices are identical, false otherwise
+ * @param {mat} a Matrix a
+ * @param {mat} b Matrix b
+ * @return {boolean} True if matrices a and b are identical, false otherwise
  */
 mat.eq = (a, b) => a.m === b.m && a.n === b.n && mat.str(a) === mat.str(b);
 
 /**
  * Copy a matrix
- * @param {mat} a
- * @return {mat} A copy of a
+ * @param {mat} a The matrix to copy
+ * @return {mat} A copy of matrix a
  */
 mat.cpy = a => mat(a.m, a.n, [...a.entries]);
 
@@ -379,18 +379,18 @@ mat.cpy = a => mat(a.m, a.n, [...a.entries]);
 
 /**
  * Call a function on each entry of a matrix and build a new matrix from the results
- * @param {mat} a
+ * @param {mat} a Matrix a
  * @param {matrixMapCallback} f The function to call on each entry of the matrix
- * @return {mat}
+ * @return {mat} Matrix a mapped through f
  */
 mat.map = (a, f) => mat(a.m, a.n, a.entries.map(f));
 
 /**
  * Convert a matrix into a string
- * @param {mat} a
+ * @param {mat} a The matrix to convert
  * @param {string} [ms=', '] The separator string for columns
  * @param {string} [ns='\n'] The separator string for rows
- * @return {string}
+ * @return {string} A string representation of the matrix
  */
 mat.str = (a, ms = ' ', ns = '\n') => a.entries.chunk(a.n).map(r => r.join(ms)).join(ns);
 
@@ -398,44 +398,44 @@ mat.str = (a, ms = ' ', ns = '\n') => a.entries.chunk(a.n).map(r => r.join(ms)).
 
 /**
  * Check if two numbers are approximately equal
- * @param {number} a
- * @param {number} b
+ * @param {number} a Number a
+ * @param {number} b Number b
  * @param {number} [p=Number.EPSILON] The precision value
- * @return {boolean}
+ * @return {boolean} True if numbers a and b are approximately equal
  */
 Math.floatEquals = (a, b, p = Number.EPSILON) => Math.abs(a - b) < p;
 
 /**
  * Clamp a number between min and max
- * @param {number} a
+ * @param {number} a The number to clamp
  * @param {number} min The minimum value
  * @param {number} max The maximum value
- * @return {number}
+ * @return {number} A clamped number
  */
 Math.clamp = (a, min = 0, max = 1) => a < min ? min : (a > max ? max : a);
 
 /**
  * Get the fractional part of a number
- * @param {number} a
- * @return {number}
+ * @param {number} a The number from which to get the fractional part
+ * @return {number} The fractional part of the number
  */
 Math.frac = a => a >= 0 ? a - Math.floor(a) : a - Math.ceil(a);
 
 /**
  * Do a linear interpolation between a and b
- * @param {number} a
- * @param {number} b
- * @param {number} i The interpolation value
- * @return {number}
+ * @param {number} a The minimum number
+ * @param {number} b The maximum number
+ * @param {number} i The interpolation value, should be in the interval [0, 1]
+ * @return {number} An interpolated value in the interval [a, b]
  */
 Math.lerp = (a, b, i) => a + (b - a) * i;
 
 /**
  * Get the position of i between a and b
- * @param {number} a
- * @param {number} b
- * @param {number} i
- * @return {number}
+ * @param {number} a The minimum number
+ * @param {number} b The maximum number
+ * @param {number} i The interpolated value in the interval [a, b]
+ * @return {number} The position of i between a and b
  */
 Math.unlerp = (a, b, i) => (i - a) / (b - a);
 
@@ -447,13 +447,13 @@ Math.unlerp = (a, b, i) => (i - a) / (b - a);
  * @param {number} c11 Bottom-right value
  * @param {number} ix Interpolation value along x
  * @param {number} iy Interpolation value along y
- * @return {number}
+ * @return {number} A bilinear interpolated value
  */
 Math.blerp = (c00, c10, c01, c11, ix, iy) => Math.lerp(Math.lerp(c00, c10, ix), Math.lerp(c01, c11, ix), iy);
 
 /**
  * Re-map a number i from range a1->a2 to b1->b2
- * @param {number} i
+ * @param {number} i The number to re-map
  * @param {number} a1
  * @param {number} a2
  * @param {number} b1
@@ -464,23 +464,24 @@ Math.remap = (i, a1, a2, b1, b2) => b1 + (i - a1) * (b2 - b1) / (a2 - a1);
 
 /**
  * Do a smooth interpolation between a and b
- * @param {*} a
- * @param {*} b
+ * @param {*} a The minimum number
+ * @param {*} b The maximum number
  * @param {*} i The interpolation value
+ * @return {number} An interpolated value in the interval [a, b]
  */
 Math.smoothstep = (a, b, i) => Math.lerp(a, b, 3 * Math.pow(i, 2) - 2 * Math.pow(i, 3));
 
 /**
  * Get an angle in radians
  * @param {number} degrees The angle in degrees
- * @return {number}
+ * @return {number} The angle in radians
  */
 Math.radians = degrees => (Math.PI / 180) * degrees;
 
 /**
  * Get an angle in degrees
  * @param {number} radians The angle in radians
- * @return {number}
+ * @return {number} The angle in degrees
  */
 Math.degrees = radians => (180 / Math.PI) * radians;
 
@@ -488,7 +489,7 @@ Math.degrees = radians => (180 / Math.PI) * radians;
  * Get a random float in the interval [min, max)
  * @param {number} min Inclusive min
  * @param {number} max Exclusive max
- * @return {number}
+ * @return {number} A random float in the interval [min, max)
  */
 Math.randomBetween = (min, max) => Math.random() * (max - min) + min;
 
@@ -496,7 +497,7 @@ Math.randomBetween = (min, max) => Math.random() * (max - min) + min;
  * Get a random integer in the interval [min, max]
  * @param {number} min Inclusive min
  * @param {number} max Inclusive max
- * @return {number}
+ * @return {number} A random integer in the interval [min, max]
  */
 Math.randomIntBetween = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 
@@ -505,7 +506,7 @@ Math.randomIntBetween = (min, max) => Math.floor(Math.random() * (max - min + 1)
  * @param {number} mu The mean value
  * @param {number} sigma The standard deviation
  * @param {number} samples The number of samples
- * @return {number}
+ * @return {number} A normally-distributed random number
  */
 Math.cltRandom = (mu = 0.5, sigma = 0.5, samples = 2) => {
   let total = 0;
@@ -519,7 +520,7 @@ Math.cltRandom = (mu = 0.5, sigma = 0.5, samples = 2) => {
  * Get a normally-distributed random integer in the interval [min, max]
  * @param {number} min Inclusive min
  * @param {number} max Inclusive max
- * @return {number}
+ * @return {number} A normally-distributed random integer
  */
 Math.cltRandomInt = (min, max) => min + Math.cltRandom(0.5, 0.5, 2) * (max - min);
 
@@ -540,10 +541,10 @@ Math.weightedRandom = w => {
 /**
  * An interpolation function
  * @callback interpolationCallback
- * @param {number} a
- * @param {number} b
- * @param {number} i The interpolation value
- * @return {number}
+ * @param {number} a The minimum number
+ * @param {number} b The maximum number
+ * @param {number} i The interpolation value, should be in the interval [0, 1]
+ * @return {number} The interpolated value in the interval [a, b]
  */
 
 /**
@@ -551,7 +552,7 @@ Math.weightedRandom = w => {
  * @param {Array<number>} a An array of values interpolate
  * @param {number} i A number in the interval [0, 1]
  * @param {interpolationCallback} [f=Math.lerp] The interpolation function to use
- * @return {number}
+ * @return {number} An interpolated value in the interval [min(a), max(a)]
  */
 Math.lerpArray = (a, i, f = Math.lerp) => {
   const s = i * (a.length - 1);
@@ -561,8 +562,8 @@ Math.lerpArray = (a, i, f = Math.lerp) => {
 
 /**
  * Get the dot product of two vectors
- * @param {Array<number>} a
- * @param {Array<number>} b
+ * @param {Array<number>} a Vector a
+ * @param {Array<number>} b Vector b
  * @return {number} a ∙ b
  */
 Math.dot = (a, b) => a.reduce((n, v, i) => n + v * b[i], 0);
@@ -573,7 +574,7 @@ Math.dot = (a, b) => a.reduce((n, v, i) => n + v * b[i], 0);
  * A function for generating array values
  * @callback timesCallback
  * @param {number} i The array index
- * @return {*}
+ * @return {*} The array value
  */
 
 /**
@@ -587,7 +588,7 @@ Array.times = (f, n) => Array(n).fill().map((_, i) => f(i));
 /**
  * Return an array containing numbers 0->(n - 1)
  * @param {number} n The size of the array
- * @return {Array<number>}
+ * @return {Array<number>} An array of integers 0->(n - 1)
  */
 Array.range = n => Array.times(i => i, n);
 
@@ -596,8 +597,8 @@ Array.range = n => Array.times(i => i, n);
  * @name at
  * @function
  * @memberof Array.prototype
- * @param {number} i
- * @return {*}
+ * @param {number} i The positively/negatively wrapped array index
+ * @return {*} An element from the array
  */
 Object.defineProperty(Array.prototype, 'at', {
   value: function (i) {
@@ -612,7 +613,7 @@ Object.defineProperty(Array.prototype, 'at', {
  * @function
  * @memberof Array.prototype
  * @param {number} n The chunk size
- * @return {Array<Array<*>>}
+ * @return {Array<Array<*>>} An array of array chunks
  */
 Object.defineProperty(Array.prototype, 'chunk', {
   value: function (n) {
